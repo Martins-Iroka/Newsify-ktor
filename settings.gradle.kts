6f43566a8ee88455daf18431b44fd0a6dc646536
Ikechukwu Iroka <martins.iroka@gmail.com>
2026-04-12 20:03:43 +0100

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "Newsify-ktor"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/HTTP.kt
@@ -0,0 +1,30 @@
+package com.martdev
+
+import io.ktor.http.*
+import io.ktor.openapi.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.*
+
+fun Application.configureHTTP() {
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader("MyCustomHeader")
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+    routing {
+        swaggerUI(path = "openapi") {
+            info = OpenApiInfo(title = "My API", version = "1.0.0")
+        }
+    }
+}
